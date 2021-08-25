bdf76c55412b3f10787a9eff0cf9ad931cd8d269
BernardoVindex <ber.cardenas94@gmail.com>
2021-08-24 23:19:20 -0500

    Avances JS

--- Platzi/JavaScript/Js_Basic/CalculadoraProcentajes/calcPor.js
+++ Platzi/JavaScript/Js_Basic/CalculadoraProcentajes/calcPor.js
@@ -1,5 +1,62 @@
-const precioOriginal = 100;
-const descento = 5;
+//const precioOriginal = 100;
+//const descuento = 5;
 
-const porcentajePrecioConDescuento = precioOriginal - descuento;
-const precio
+const cupones = [
+    {nombre: "DescuentoVerano", descuento: 10},
+    {nombre: "DescuentoOtoño", descuento: 20},
+    {nombre: "DescuentoPrimavera",descuento: 15},
+    {nombre: "DescuentoInvierno", descuento: 30}
+]
+
+function calcularPrecioConDescuento(precio, descuento) {    
+    const porcentajePrecioConDescuento = 100 - descuento ;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;     
+
+    return precioConDescuento;
+}
+
+function calcularDescuentoCupon(inputCupon,precio) {
+   
+
+   if(inputCupon = false) {
+        descuentoConCupon = 0;
+   }
+   else if(inputCupon != cupones.nombre) {
+       alert("Cupon no valido");
+   } 
+   else {
+        const tipoDeCupon = cupones.filter(function(cupones) {
+            return cupones.nombre == inputCupon
+    }); 
+    const descuentoConCupon =  precio * (tipoDeCupon.descuento) / 100;
+   }
+   
+   return descuentoConCupon;    
+}
+
+
+function onClickButtonPriceDisscount(){
+    const inputPrice = document.getElementById("InputPrice")
+    const priceValue = inputPrice.value;
+
+    const inputDisscount = document.getElementById("InputDisscount")
+    const disscountValue = inputDisscount.value;
+
+    const inputCupon = document.getElementById("InputCupon")
+    
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, disscountValue); 
+    const descuentoConCupon = calcularDescuentoCupon(inputCupon, priceValue)
+    
+    const precioConDescuentoTotal = precioConDescuento - descuentoConCupon;
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuentoTotal;
+}
+
+/*s
+console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento
+}*/
