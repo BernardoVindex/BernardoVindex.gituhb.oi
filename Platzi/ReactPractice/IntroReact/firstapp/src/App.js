5f494d7b1c0a67619f129d5c364d105a3a7032fd
BernardoVindex <ber.cardenas94@gmail.com>
2021-11-17 17:19:16 -0600

    Iniciando con React

---
+++ Platzi/ReactPractice/IntroReact/firstapp/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { TodoCounter } from './TodoCounter'
+import { CreateTodoButton } from './CreateTodoButton'
+import { TodoItem } from './TodoItem'
+import { TodoList } from './TodoList'
+import { TodoSearch } from './TodoSearch'
+//import './App.css';
+
+const todos = [
+  {text: 'Cortar cebolla', completed: false},
+  {text: 'Tomar curso de Intro a React', completed: false},
+  {text: 'Llorar con la llorona', completed: false},
+]
+
+function App() {
+  return (
+    <React.Fragment>
+
+      <TodoCounter />
+      
+      <TodoSearch />
+      
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+      
+    </React.Fragment>
+  );
+}
+
+export default App;
